753288131c3e181c25cac057d13c803d60d337c9
davidenq <davip@live.com>
2015-09-23 03:34:42 -0500

    actualizando index.js

--- index.js
+++ index.js
@@ -4,24 +4,15 @@ var color = require('./src/color');
 var buffer = require('./src/helpers/loadBuffer');
 
 //Cargando buffer de las imágenes
-var bufferImg1 = buffer.get('4');
-var bufferImg2 = buffer.get('3');
+var bufferImg = buffer.get('especificar_el_nombre_de_la_imagen');
 
-
+//Ejemplos
 color.generate({
-    buffer: {
-        left: bufferImg1,
-        right: bufferImg2
-    },
-    type: '3D'
-});
-
-color.generate({
-    buffer: bufferImg1,
+    buffer: bufferImg,
     model: 'yuv'
 });
 
 color.generate({
-    buffer: bufferImg1,
+    buffer: bufferImg,
     color: 'blue'
 });
